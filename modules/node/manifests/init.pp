436cc6ccc89df188101afef749743d51dd8c0f79
Peter Carrero <peter.carrero@gmail.com>
2014-06-12 20:23:31 -0500

    Add nodejs packages to puppet

---
+++ modules/node/manifests/init.pp
@@ -0,0 +1,9 @@
+class node {
+	if $operatingsystem == 'CentOS' and $operatingsystemmajrelease == '6' {
+		$nodePackages = [ 'nodejs', 'npm' ]
+		package { $nodePackages:
+			ensure => installed,
+			require => $commonTools::epel_package,
+		}
+	}
+}
