6ed51ac9632a97244d40a9305a06a8561b7104db
Peter Carrero <peter.carrero@gmail.com>
2014-06-12 20:55:06 -0500

    Nodejs class setup, take2

---
+++ modules/perfmon/manifests/init.pp
@@ -0,0 +1,7 @@
+class perfomn {
+	$perfPackages = [ 'grunt', 'grunt-cli', 'phantom', 'grunt-phantom', 'phantomas', 'yslow', 'slimerjs', 'wraith', ]
+	package { $perfPackages:
+		ensure => installed,
+		provider => 'npm',
+	}
+}
