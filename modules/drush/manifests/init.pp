c030b691ce43cade176a8e4ffcb0d18abc42b2ec
Peter Carrero <peter.carrero@gmail.com>
2014-07-03 09:49:06 -0500

    Make drush modules conditional on dbfile fact

--- modules/drush/manifests/init.pp
+++ modules/drush/manifests/init.pp
@@ -46,9 +46,11 @@ class drush {
 		require => Exec [ 'drush' ],
 	}
 
-	exec { 'drush_enable_modules':
-		command => "/usr/local/bin/drush -r $webroot en stage_file_proxy devel xhprof memcache memcache_admin",
-		require => [ File [ '/usr/local/bin/drush'], Service [ $lamp::dbservice ] ],
+	if defined('$dbfile') {
+		exec { 'drush_enable_modules':
+			command => "/usr/local/bin/drush -r $webroot en stage_file_proxy devel xhprof memcache memcache_admin",
+			require => [ File [ '/usr/local/bin/drush'], Service [ $lamp::dbservice ], Exec [ 'setup_dbfile' ], ],
+		}
 	}
 
 	filehttp { 'set_prompt.sh':
