f5e9e50a3e8c23ee28ed823316cfa0c943bb8176
Peter Carrero <peter.carrero@gmail.com>
2014-09-02 14:40:55 -0500

    Account for missing path on vagrant user

--- data/fixmysql.bash
+++ data/fixmysql.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-MySQLOK=`pidof mysqld > /dev/null; echo $?`
+MySQLOK=`/sbin/pidof mysqld > /dev/null; echo $?`
 if [ $MySQLOK -eq 1 ]; then
   rm /var/lib/mysql/mysql.sock
   /etc/init.d/mysql51-mysqld start
