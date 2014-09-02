c831aa4d81d3b7ab5f59bb48c7c1c79fff08356c
Peter Carrero <peter.carrero@gmail.com>
2014-09-02 10:19:31 -0500

    Use proper logic in if statement

--- data/fixmysql.bash
+++ data/fixmysql.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 MySQLOK=`pidof mysqld > /dev/null; echo $?`
-if [ $MySQLOK -eq 0 ]; then
+if [ $MySQLOK -eq 1 ]; then
   rm /var/lib/mysql/mysql.sock
   /etc/init.d/mysql51-mysqld start
 fi
