1f64ab3638e3bf5f1d649b22eb5768ede86a1db5
Peter Carrero <peter.carrero@gmail.com>
2015-01-16 15:32:35 -0600

    drush: Remove devel_themer from build
    - this may cause more troubles than benefits by being on by default

--- modules/drush/manifests/init.pp
+++ modules/drush/manifests/init.pp
@@ -48,7 +48,7 @@ class drush {
 
 	if defined('$dbfile') {
 		exec { 'drush_enable_modules':
-			command => "/usr/local/bin/drush -r $webroot -y en stage_file_proxy devel xhprof memcache memcache_admin update language_domains devel_themer",
+			command => "/usr/local/bin/drush -r $webroot -y en stage_file_proxy devel xhprof memcache memcache_admin update language_domains",
 			require => defined('$redodb') ? {
 				false => [ File [ '/usr/local/bin/drush'], Service [ $lamp::dbservice ], Exec [ 'setup_dbfile' ], ],
 				true  => [ File [ '/usr/local/bin/drush'], Service [ $lamp::dbservice ], Exec [ 'reprovision_dbfile' ], ],
